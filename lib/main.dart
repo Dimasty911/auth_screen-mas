5d72f7cc0651dbf8f35cd4d1a7579170c4d672d8
Admin <DESKTOP-F6DFEEC>
2022-02-11 22:21:06 +0300

    Загрузка данных по сети

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,8 @@ import 'bottom_bar.dart';
 import 'theme_demo_screen.dart';
 import 'listview.dart';
 import 'switches_demo.dart';
-
 import 'files_demo3.dart';
+import 'networking.dart';
 void main() {
-  runApp(const FilesDemoScreen());
+  runApp(const NetworkingScreen());
 }
